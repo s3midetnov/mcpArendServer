9337b3d7e620822d2249c50ee794b2a272aa792e
Artem Semidetnov <artemsemidetnov@gmail.com>
2026-01-02 12:42:20 +0000

    added a call to ArendClient for typechecking

--- src/main/kotlin/arendMCP/McpServer.kt
+++ src/main/kotlin/arendMCP/McpServer.kt
@@ -1,6 +1,7 @@
 package org.example.arendMCP
 
 import io.modelcontextprotocol.kotlin.sdk.CallToolResult
+import io.modelcontextprotocol.kotlin.sdk.Tool
 import io.modelcontextprotocol.kotlin.sdk.Implementation
 import io.modelcontextprotocol.kotlin.sdk.ServerCapabilities
 import io.modelcontextprotocol.kotlin.sdk.TextContent
@@ -12,6 +13,12 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.io.asSink
 import kotlinx.io.asSource
 import kotlinx.io.buffered
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.putJsonObject
+import org.example.arendClient.ArendClient
+import org.example.arendClient.ArendClientImpl
 
 fun main() {
     val server: Server = createServer()
@@ -32,16 +39,30 @@ fun createServer(): Server {
         "Arend_goal_typecheck_MCP",
         "1.0.0"
     )
-    val options = ServerOptions(capabilities = ServerCapabilities(tools = ServerCapabilities.Tools(true))
+    val options = ServerOptions(
+        capabilities = ServerCapabilities(tools = ServerCapabilities.Tools(true))
     )
     val server = Server(info, options)
+    val arendClient : ArendClient = ArendClientImpl()
+
+    val codeInputSchema = Tool.Input(
+        buildJsonObject {
+            putJsonObject("function"){JsonPrimitive("string")}
+        }
+    )
 
     server.addTool(
         name="Typecheck_definition",
         description = "Typechecks a definition in Arend, returns error messages separated by comma",
-    ){ CallToolResult(
-        listOf(TextContent("No errors!"))
-    ) }
+        codeInputSchema
+    )
+    { input ->
+        val function = input.arguments["function"]!!.jsonPrimitive.content
+        CallToolResult(
+        listOf(
+            TextContent(arendClient.typecheck(function)))
+        )
+    }
 
     return server
 }
